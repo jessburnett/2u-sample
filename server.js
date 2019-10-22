36a1782652d0a27d7aca3feb63a401e339ba7bb3
jessburnett <jess.a.burnett@gmail.com>
2019-10-22 14:00:48 -0400

    backend routes auth, users, courses

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => res.json({message: 'welcome 2u web server'}));
 
+//define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/courses', require('./routes/courses'));
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
