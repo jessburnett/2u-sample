02fa85c4c0af5e42a0dd72f891c52ab1b0f3678a
jessburnett <jess.a.burnett@gmail.com>
2019-10-22 15:02:15 -0400

    user validation and test

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/Users');
 
@@ -8,8 +9,19 @@ const User = require('../models/Users');
 // @access Public
 // @postman test: http://localhost:5000/api/users
 
-router.post('/', (req, res)=> {
-  res.send(req.body);
+router.post('/', 
+//validation  
+  [
+    check('name', 'Please add your name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+  ],
+  (req, res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('passed');
 });
 
 module.exports = router;
