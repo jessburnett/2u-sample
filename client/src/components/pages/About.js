67aa673d1e8568b8c4c1ad11af3763c07ad9ec43
jessburnett <jess.a.burnett@gmail.com>
2019-10-23 14:02:59 -0400

    navbar, routes, home, about

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className="my-1">
+        This is a full stack for 2U demonstration of my skills. After consulting all year and not being hands on daily, I decided to dive in and get the rust of the gears and grind this out.
+      </p>
+      <p className="bg-dark p">
+        <strong>Version 1.0.0</strong><br/>
+        <strong>Author: Jess Burnett</strong>
+      </p>
+    </div>
+  )
+}
+
+export default About
